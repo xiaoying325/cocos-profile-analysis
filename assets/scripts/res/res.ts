fe24807d55d816342a25c31b30246da5516833f1
pengchao <pengchao@wepie.com>
2025-10-29 11:31:23 +0800

    fix:[main]

--- assets/scripts/res/res.ts
+++ assets/scripts/res/res.ts
@@ -7,13 +7,13 @@ export default class res extends cc.Component {
 
     start() {
 
-        // cc.assetManager.loadAny('configs/gifconf', { __requestType__: "path", type: null, bundle: 'resources', __outputAsArray__: Array.isArray("configs/gifconf") }, (err, asset) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     }
-        //     console.log(asset);
-        // })
+        cc.assetManager.loadAny('configs/gifconf', { __requestType__: "path", type: null, bundle: 'resources', __outputAsArray__: Array.isArray("configs/gifconf") }, (err, asset) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(asset);
+        })
 
     }
 
